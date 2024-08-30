4bd5c3a2f319f866bb7bbedcc73cc076cc674f15
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-29 20:53:52 -0400

    add base project

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.paperweight)
+    alias(libs.plugins.shadowJar)
+    alias(libs.plugins.runPaper)
+
+    `paper-plugin`
+}
+
+rootProject.version = "1.0.0"
+
+repositories {
+    maven("https://repo.codemc.io/repository/maven-releases")
+
+    maven("https://repo.fancyplugins.de/releases")
+}
+
+dependencies {
+    paperweight.paperDevBundle(libs.versions.paper)
+
+    implementation(libs.vital.paper)
+
+    compileOnly(libs.placeholderapi)
+
+    compileOnly(libs.packet.events)
+
+    compileOnly(libs.hikari.cp)
+
+    compileOnly(libs.yardwatch)
+}
+
+tasks {
+    runServer {
+        jvmArgs("-Dnet.kyori.ansi.colorLevel=truecolor")
+
+        defaultCharacterEncoding = Charsets.UTF_8.name()
+
+        minecraftVersion(libs.versions.minecraft.get())
+    }
+
+    shadowJar {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+
+        listOf(
+            "com.ryderbelserion.vital"
+        ).forEach {
+            relocate(it, "libs.$it")
+        }
+    }
+
+    processResources {
+        inputs.properties("name" to rootProject.name)
+        inputs.properties("version" to project.version)
+        inputs.properties("group" to project.group)
+        inputs.properties("apiVersion" to libs.versions.minecraft.get())
+        inputs.properties("description" to project.properties["description"])
+        inputs.properties("website" to project.properties["website"])
+
+        filesMatching("paper-plugin.yml") {
+            expand(inputs.properties)
+        }
+    }
+}
