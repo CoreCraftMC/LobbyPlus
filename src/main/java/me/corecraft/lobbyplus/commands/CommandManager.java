bd40c9dd947b0aeb36aa1811887dd18c924e05e1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-30 02:58:07 -0400

    add bypass command

--- src/main/java/me/corecraft/lobbyplus/commands/CommandManager.java
+++ src/main/java/me/corecraft/lobbyplus/commands/CommandManager.java
@@ -4,22 +4,26 @@ import com.ryderbelserion.vital.paper.api.builders.PlayerBuilder;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
 import me.corecraft.lobbyplus.LobbyPlus;
+import me.corecraft.lobbyplus.api.cache.UserManager;
+import me.corecraft.lobbyplus.api.enums.types.BypassType;
 import me.corecraft.lobbyplus.commands.relations.ArgumentRelations;
 import me.corecraft.lobbyplus.commands.subs.BaseCommand;
 import me.corecraft.lobbyplus.commands.subs.player.CommandHelp;
+import me.corecraft.lobbyplus.commands.subs.staff.CommandBypass;
 import me.corecraft.lobbyplus.commands.subs.staff.CommandReload;
-import me.corecraft.lobbyplus.configs.ConfigManager;
 import me.corecraft.lobbyplus.configs.impl.messages.MiscKeys;
-import me.corecraft.lobbyplus.utils.MsgUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CommandManager {
 
     private final static @NotNull LobbyPlus plugin = LobbyPlus.get();
+
+    private final static @NotNull UserManager userManager = plugin.getUserManager();
 
     private final static @NotNull BukkitCommandManager<CommandSender> commandManager = BukkitCommandManager.create(plugin);
 
@@ -32,10 +36,12 @@ public class CommandManager {
         commandManager.registerSuggestion(SuggestionKey.of("pages"), (sender, context) -> {
             final List<String> numbers = new ArrayList<>();
 
-            ConfigManager.getLocale(MsgUtils.getLocale(sender)).getProperty(MiscKeys.help).getEntry().keySet().forEach(value -> numbers.add(String.valueOf(Integer.parseInt(value))));
+            userManager.getUser(sender).getLocale().getProperty(MiscKeys.help).getEntry().keySet().forEach(value -> numbers.add(String.valueOf(Integer.parseInt(value))));
 
             return numbers;
         });
+
+        commandManager.registerSuggestion(SuggestionKey.of("bypass_type"), (sender, context) -> Arrays.stream(BypassType.values()).map(BypassType::getName).toList());
 
         // default
         commandManager.registerSuggestion(SuggestionKey.of("players"), (sender, context) -> plugin.getServer().getOnlinePlayers().stream().map(Player::getName).toList());
@@ -55,6 +61,7 @@ public class CommandManager {
                 new BaseCommand(),
 
                 new CommandReload(),
+                new CommandBypass(),
                 new CommandHelp()
         ).forEach(commandManager::registerCommand);
     }
