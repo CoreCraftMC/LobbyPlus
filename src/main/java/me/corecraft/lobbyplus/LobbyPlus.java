fa757d1f51f7c81fe69af0be8ba1c60272f1beea
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-30 03:11:27 -0400

    fuck whitespace

--- src/main/java/me/corecraft/lobbyplus/LobbyPlus.java
+++ src/main/java/me/corecraft/lobbyplus/LobbyPlus.java
@@ -19,7 +19,6 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 import java.util.Locale;
 
